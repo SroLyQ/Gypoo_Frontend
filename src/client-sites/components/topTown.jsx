48e234ef949ef9d4214e58a1bb4ce5a1e51942d8
Chayut Phunpeth <chayutphunpeth@phaiez.local>
2022-11-11 09:41:02 +0700

    main tor

---
+++ src/client-sites/components/topTown.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function topTown() {
+  return (
+    <div className="my-10 ">
+      <p className=" font-kanit mx-[270px]">จังหวัดยอดฮิต</p>
+      <div className="flex mx-auto rounded-2xl w-[900px] bg-slate-200 py-7">
+        dwdw
+      </div>
+    </div>
+  );
+}
+
+export default topTown;
