2abc92927d11f69a93c31855be6461d50867940e
kawjaw66 <kawjaw66@gmail.com>
2022-11-12 14:16:24 +0700

    **

--- src/App.tsx
+++ src/App.tsx
@@ -14,8 +14,6 @@ import HotelDiscount from './hotel-sites/pages/HotelDiscount';
 import HotelHistory from './hotel-sites/pages/HotelHistory';
 import HotelInnerCard from './hotel-sites/components/HotelInnerCard';
 import testdata from './hotel-sites/pages/testdata.json';
-import TestPage from './component/TestPage';
-
 
 function App() {
   return (
@@ -28,15 +26,14 @@ function App() {
           <Route path="/hotel" element={<Hotel />} />
           <Route path="/restaurant" element={<Restaurant />} />
           <Route path="/activity" element={<Activity />} />
-          <Route path='/hotelhotels' element = {<HotelHotels/>}/>
-          <Route path='/hotelrestaurants' element = {<HotelRestaurants/>}/>
-          <Route path='/addhotel' element = {<AddHotel/>}/>
-          <Route path='/hotelhistory' element = {<HotelHistory/>}/>
-          <Route path='/hotelprofile' element = {<HotelProfile/>}/>
-          <Route path='/hoteldiscount' element = {<HotelDiscount/>}/>
-          <Route path ='/hotelhome' element = {<HotelHome/>} /> 
-          <Route path ='/hotel/:_id' element = {<HotelInnerCard />} /> 
-
+          <Route path="/hotelhotels" element={<HotelHotels />} />
+          <Route path="/hotelrestaurants" element={<HotelRestaurants />} />
+          <Route path="/addhotel" element={<AddHotel />} />
+          <Route path="/hotelhistory" element={<HotelHistory />} />
+          <Route path="/hotelprofile" element={<HotelProfile />} />
+          <Route path="/hoteldiscount" element={<HotelDiscount />} />
+          <Route path="/hotelhome" element={<HotelHome />} />
+          <Route path="/hotel/:_id" element={<HotelInnerCard />} />
         </Routes>
       </BrowserRouter>
     </>
